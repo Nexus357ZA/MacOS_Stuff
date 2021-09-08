41c85c970e591a4e6b946af2f9e316dda9597d45
Nexus357ZA <stefan.de.kock@lockwood-publishing.com>
2021-09-08 16:16:19 +0100

    Installation script for my commonly used packages

---
+++ homebrew/install_stuff.sh
@@ -0,0 +1,18 @@
+#!/usr/local/bin/zsh
+
+brew install ansible
+brew install awscli
+brew install gcc
+brew install git
+brew install grep
+brew install helm
+brew install consul
+brew install jfrog-cli
+brew install packer
+brew install pipx
+brew install pyenv
+brew install tfenv
+brew install vault
+brew install waypoint
+brew install aws-vault
+brew install vagrant
